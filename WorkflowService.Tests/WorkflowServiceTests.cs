efa6952a355d6632d003d16fd1975ef04c4a4b29
anthonymohamedap <s173186@ap.be>
2026-02-28 17:08:52 +0100

    fux

--- WorkflowService.Tests/WorkflowServiceTests.cs
+++ WorkflowService.Tests/WorkflowServiceTests.cs
@@ -7,6 +7,7 @@ using QuadroApp.Service.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
